2455e5bcc85b3407218443365c33c16bc1d93b12
Joseunion <jose23_villa@hotmail.es>
2021-04-08 13:50:40 +0200

    Desarrollo del listado de arbitros para la aplicacion web

---
+++ src/main/java/com/sanvalero/townleague/appwebcontroller/RefereeAppWebController.java
@@ -0,0 +1,33 @@
+package com.sanvalero.townleague.appwebcontroller;
+
+import com.sanvalero.townleague.domain.Referee;
+import com.sanvalero.townleague.service.RefereeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+import java.util.Set;
+
+@Controller
+class RefereeAppWebController {
+
+    @Autowired
+    private RefereeService refereeService;
+
+    @RequestMapping(value = "/")
+    public String index(Model model) {
+        model.addAttribute("inicio", "INDEX");
+        return "index";
+    }
+
+    @RequestMapping(value = "/web-referees", method = RequestMethod.GET)
+    public String referee(Model model) {
+        Set<Referee> referees = refereeService.findAllReferees();
+        model.addAttribute("referee", referees);
+        return "referee";
+    }
+
+}

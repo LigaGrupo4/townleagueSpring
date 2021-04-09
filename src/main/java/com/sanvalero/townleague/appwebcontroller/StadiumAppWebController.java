76984bf90c3e649427c5b39d9ea69480a14ddfab
Joseunion <jose23_villa@hotmail.es>
2021-04-09 15:59:36 +0200

    Se añaden estilos

--- src/main/java/com/sanvalero/townleague/appwebcontroller/StadiumAppWebController.java
+++ src/main/java/com/sanvalero/townleague/appwebcontroller/StadiumAppWebController.java
@@ -4,12 +4,14 @@ import com.sanvalero.townleague.domain.Referee;
 import com.sanvalero.townleague.domain.Stadium;
 import com.sanvalero.townleague.service.StadiumService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Set;
 
+@Controller
 public class StadiumAppWebController {
 
     @Autowired
